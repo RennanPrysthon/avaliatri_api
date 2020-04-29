3ff24df9f6b9c8888719eaf769bf3ee9c0e68185
Rennan Prysthon <rennandelcastillo@gmail.com>
2020-04-29 16:54:52 -0300

    Atualizando resposta da prova

--- src/main/java/br/avaliatri/dtos/util/QuestaoRespondidaDTO.java
+++ src/main/java/br/avaliatri/dtos/util/QuestaoRespondidaDTO.java
@@ -1,19 +1,20 @@
 package br.avaliatri.dtos.util;
 
 import br.avaliatri.dtos.AlternativaDTO;
-import br.avaliatri.dtos.QuestaoDTO;
 import br.avaliatri.models.Alternativa;
 import br.avaliatri.services.AlternativaService;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class QuestaoRespondidaDTO {
     @NotNull(message = "Insira o id da questao") private Integer questao;
     @NotNull(message = "Insira a alternativa") private String alternativa_usuario;
-    private List<AlternativaDTO> alternativas = null;
+    private List<AlternativaDTO> alternativas;
     private Boolean is_correta;
 
     public QuestaoRespondidaDTO() {
